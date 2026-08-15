94fca4622ce8198e34bf6a56a5a316ca0046f4c5
Robert Cowher <bobcowher@gmail.com>
2026-08-15 16:00:20 -0500

    Updated to 100K epochs

--- scripts/train.py
+++ scripts/train.py
@@ -6,7 +6,7 @@ from agent import Agent
 # agent = Agent(data_path="dataset/microwave", name="bc_microwave")
 agent = Agent(data_path="dataset", name="bc_network")
 
-epochs = 10001
-# epochs = 100001
+# epochs = 10001
+epochs = 100001
 
 agent.train(epochs=epochs, batch_size=64)
